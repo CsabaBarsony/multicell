5e07c8294e168f2a799ecc686de1a39e0b07b096
CsabaBarsony <csaba.barsony@gmail.com>
2016-07-07 08:23:41 +0200

    init

---
+++ MultiCell/World/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCell.World
+{
+    class Cell
+    {
+        Guid Id { get; set; }
+        List<Cell> Connections;
+        Cell Parent;
+        DNA DNA;
+        int Age = 0;
+        int NumberOfFissions = 0;
+        float Energy = 1.0f;
+
+        public Cell()
+        {
+            Id = Guid.NewGuid();
+            DNA = new DNA();
+            Connections = new List<Cell>();
+        }
+
+        public void IncreaseAge()
+        {
+            this.Age += 1;
+        }
+    }
+}
---
+++ MultiCell/World/Incubator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCell.World
+{
+    class Incubator
+    {
+        public List<Cell> Cells { get; set; }
+
+        public Incubator()
+        {
+            Cells = new List<Cell>();
+            Cells.Add(new Cell());
+        }
+
+        public void StartIncubation()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+            }
+        }
+    }
+}
